59b9166b86c1db7f61352353bd909e091013813e
Alexey Barsukov <albar3000@yandex.ru>
2020-04-28 22:46:06 +0300

    new

--- scripts/script.js
+++ scripts/script.js
@@ -2,31 +2,30 @@ const popup = document.querySelector('.popup'),
     popupOpen = document.querySelector('.profile__edit_button'),
     popupClose = document.querySelector('.popup__close_button'),
     popupSubmit = document.querySelector('.popup__submit_button'),
-    formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.profile__title'),
+    formElement = document.querySelector('.popup__form'),
+    nameInput = document.querySelector('.profile__title');
     jobInput = document.querySelector('.profile__subtitle'),
     fieldName = document.querySelector('.popup__field_name'),
     fieldAbout = document.querySelector('.popup__field_about');
 
     
-function formOpenClose (evt) {
-    popup.classList.add('popup__opened');
+function formOpenClose () {
+    popup.classList.toggle('popup_opened');
     
     
-    if (popup.classList.contains('popup__opened')){
+    if (popup.classList.contains('popup_opened')) {
         fieldName.value = nameInput.textContent;
     fieldAbout.value = jobInput.textContent;
         
-}else {
-    nameInput.textContent = fieldName.value;
-        jobInput.textContent = fieldAbout.value;   
-    
-}
+    }else {
+    nameInput.value = nameInput.textContent;
+        jobInput.value = jobInput.textContent;   
+    }
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    popup.classList.remove('popup__opened');
+    formOpenClose ();
     nameInput.textContent = fieldName.value;
         jobInput.textContent = fieldAbout.value;
 }
